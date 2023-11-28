59d70c1f60d9bd5e681892a20ba05c192a9118e3
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-28 13:47:09 -0500

    seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
-// STRETCH
+const cars = [
+    {
+        vin: '1111111',
+        make: 'toyota',
+        model: 'prius',
+        milage: 21500,
+        title: 'clean',
+        transmisson: 'manual'
+    },
+    {
+        vin: '1111111',
+        make: 'toyota',
+        model: 'corolla',
+        milage: 1500,
+        title: 'salvage',
+
+    },
+    {
+        vin: '1111111',
+        make: 'toyota',
+        model: '4Runner',
+        milage: 100000,
+    },
+    {
+        vin: '1111111',
+        make: 'ford',
+        model: 'focus',
+        milage: 2150,
+    },
+
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
