e8760f50a9753b395b5ff6d15d5eef11a929c1c4
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-28 20:49:39 -0500

    done

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,6 +1,6 @@
 const cars = [
     {
-        vin: '1111111111111',
+        vin: '1GCEC14H4DF123456',
         make: 'toyota',
         model: 'prius',
         mileage: 21500,
@@ -8,7 +8,7 @@ const cars = [
         transmission: 'manual'
     },
     {
-        vin: '2222222222222',
+        vin: 'JHMGE8H30Xs123456',
         make: 'toyota',
         model: 'corolla',
         mileage: 1500,
@@ -16,13 +16,13 @@ const cars = [
 
     },
     {
-        vin: '3333333333333',
+        vin: '5YJ3E1EAXJF001234',
         make: 'toyota',
         model: '4Runner',
         mileage: 100000,
     },
     {
-        vin: '4444444444444',
+        vin: 'WBA3B5G57FNS12345',
         make: 'ford',
         model: 'focus',
         mileage: 2150,
